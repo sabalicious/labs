83dda60f261054bbf34a5ceb9fffddaeb748a808
sabalicious <vladchermenin4@gmail.com>
2024-03-04 18:47:05 +0300

    first commit

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
-#include "libs/data_structures/unordered_set/unordered_set.h"
 #include <stdio.h>
 
-#define MAX_SERIES 100000
-
 int main() {
-    int n;
-    scanf("%d", &n);
+	printf("ffdf");
 
-    unordered_array_set watched_series = unordered_array_set_create(MAX_SERIES);
-
-    long long expected_sum = (long long)n * (n + 1) / 2;
-    long long sum_watched = 0;
-
-    for (int i = 1; i < n; ++i) {
-        int watched;
-        scanf("%d", &watched);
-        unordered_array_set_insert(&watched_series, watched);
-        sum_watched += watched;
-    }
-
-    long long missed_episode = expected_sum - sum_watched;
-
-    printf("%lld\n", missed_episode);
-
-    unordered_array_set_delete(watched_series);
-
-    return 0;
+	return 0;
 }
 
 
