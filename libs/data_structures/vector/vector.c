5e3ae6ece71cd04a9e9d22470309bb8107b9a44e
sabalicious <vladchermenin4@gmail.com>
2024-06-05 18:16:23 +0300

    vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -14,6 +14,7 @@ typedef struct vector {
     int *data; // указатель на элементы вектора
     size_t size; // размер вектора
     size_t capacity; // вместимость вектора
+    size_t capacity; // вместимость вектора
 } vector;
 
 //проверка на то, является ли вектор полным
