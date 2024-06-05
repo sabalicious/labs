63773e9ed0cb4c4f58fd85e8caf73fe001a140f8
sabalicious <vladchermenin4@gmail.com>
2024-06-05 18:10:09 +0300

    vectot

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,5 +1,13 @@
+#ifndef INC_VECTOR_H
+#define INC_VECTOR_H
+
+#include <stdint.h>
+#include <assert.h>
+#include <memory.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include "../../algorithms/array/array.c"
 
 typedef struct vector {
     int *data; // указатель на элементы вектора
@@ -7,4 +15,47 @@ typedef struct vector {
     size_t capacity; // вместимость вектора
 } vector;
 
+
+//возвращает структуру-дескриптор вектор из n значений.
 vector createVector(size_t n);
+
+//изменяет количество
+//памяти, выделенное под хранение элементов вектора.
+void reserve(vector *v, size_t newCapacity);
+
+//удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector *v);
+
+//освобождает память, выделенную под
+//неиспользуемые элементы.
+void shrinkToFit(vector *v);
+
+//освобождает память, выделенную вектору.
+void deleteVector(vector *v);
+
+//проверка на то, является ли вектор пустым
+bool isEmpty(vector *v);
+
+//проверка на то, является ли вектор полным
+bool isFull(vector *v);
+
+//возвращает i-ый элемент вектора v
+int getVectorValue(vector *v, size_t i);
+
+//добавляет элемент в конец вектора
+void pushBack(vector *v, int x);
+
+//у3даляет элемент с конца вектора
+void popBack(vector *v);
+
+// возвращает указатель на
+//index-ый элемент вектора.
+int* atVector(vector *v, size_t index);
+
+//возвращает указатель на последний элемент вектора.
+int* back(vector *v);
+
+//возвращает указатель на нулевой элемент вектора.
+int* front(vector *v);
+
+# endif
